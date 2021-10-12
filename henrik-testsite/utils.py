8fb3b1b78d7e291e74bd561504696f6cc06b0bd8
henrikfjell <h1rik.f@gmail.com>
2021-10-12 10:45:57 +0200

    polar coordinates

--- henrik-testsite/utils.py
+++ henrik-testsite/utils.py
@@ -7,7 +7,8 @@ import random
 import math
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
+from scipy import stats
+import numpy as np
 
 # Train
 class PrintDot(tf.keras.callbacks.Callback):
@@ -19,122 +20,45 @@ def plot_history(hist):
     plt.figure()
     plt.xlabel('Epoch')
     plt.ylabel('MSLE')
+    plt.yscale("log")
     plt.plot(hist['epoch'], hist['msle'], label='Train Error')
     plt.plot(hist['epoch'], hist['val_msle'], label = 'Val Error')
     plt.legend()
 
-def preProcessData(metaData : dict, data : pd.DataFrame):
-    for index, row in data.iterrows():
-        pass
+def preProcess_numericalData(features, train, test, outliers_value=7):
+    # Outlayer removal
+    no_outlayers = train[(np.abs(stats.zscore(train[["area_total", "price"]])) < outliers_value).all(axis=1)] 
 
-    
-def stopLoop(stop):
-    input()
-    stop[0]=True
+    # Labels and targets
+    labels1 = no_outlayers[features]
+    labels1 = labels1.fillna(labels1.mean())
+    targets1= no_outlayers['price'] # Non nan values here.
 
-def rangeProgressBar(start, end=None, cancellable=False):
-    def pad(s,num,pad):
-        l = max(num-len(s),0)
-        return s + "".join([pad for i in range(l)])
-    
-    if end is None:
-        end = start
-        start = 0
-    startTime = time.time()
-    nextUpdateBarTime = startTime
-    
-    stop = [False]
-    if cancellable:
-        Thread(target=stopLoop, args=(stop,)).start()
-    
-    orientation = 0
-    label = ipywidgets.HTML()
-    #button = ipywidgets.Button(description="Click Me!")
-    #hBox = ipywidgets.HBox([label,button])
-    #display(hBox)
-    display(label)
-    i = 0
-    while i < end:
-        
-        if i == end-1 or time.time() >= nextUpdateBarTime:
-            nextUpdateBarTime = time.time() + 0.5 # + 1/2 second.
-            
-            if i == end-1:
-                index = end
-            else:
-                index = i
-            progress = index/end
-            
-            text = "<p style=\"font-family:'Lucida Console', monospace\">["
-            barLength = 30
-            barsDone = int(barLength*progress)
-            text += "".join(["|" for i in range(barsDone)])
-            if barLength-barsDone > 0:
-                orientation = (orientation+1) % 4
-                text += ["▄","█","▀","█"][orientation]
-                #text += ["▌","█","▐","█"][orientation]
-                text += "".join([":" for i in range(barLength-barsDone-1)])
-            text += "]&nbsp;&nbsp;"
-            
-            text += pad(str(int(progress*100)),3,'&nbsp') + "%&nbsp;&nbsp;"
-            
-            secondsPast = (time.time()-startTime)
-            expectedFinish = secondsPast / ((1 if progress == 0 else progress))
-            timeLeft = expectedFinish - secondsPast
-            text += "Elapsed: {}:{}:{}&nbsp;&nbsp;Total: {}:{}:{}&nbsp;&nbsp;Left: {}:{}:{}".format(
-                pad(str(int(secondsPast/60/60)),2,"&nbsp;"),
-                pad(str(int(secondsPast/60)%60),2,"&nbsp;"),
-                pad(str(int(secondsPast)%60),2,"&nbsp;"),
-                pad(str(int(expectedFinish/60/60)),2,"&nbsp;"),
-                pad(str(int(expectedFinish/60)%60),2,"&nbsp;"),
-                pad(str(int(expectedFinish)%60),2,"&nbsp;"),
-                pad(str(int(timeLeft/60/60)),2,"&nbsp;"),
-                pad(str(int(timeLeft/60)%60),2,"&nbsp;"),
-                pad(str(int(timeLeft)%60),2,"&nbsp;")
-            )
-            
-            label.value = text + "</p>"
-        if stop[0]:
-            return
-        yield i
-        i += 1
+    # Test
+    test1 = test[features]
+    test1 = test1.fillna(test1.mean())
 
-def multiGraph(graphs, titles, yMax=None, yMin=0):
-    if type(graphs[0]) != list:
-        graphs = [graphs]
-    
-    maxLength = max([len(v) for v in graphs])
-    maxValue = max([max(v) for v in graphs])
-    x = np.arange(0, maxLength, 1)
-    fig, axs = plt.subplots(1, figsize=(15, 15))  # Create a figure and an axes.
-    
-    currentAxes = plt.gca()
-    if yMax is not None:
-        currentAxes.set_ylim([yMin,yMax])
-        
-    
-    def stepSize(x):
-        density = 20
-        thenth = 10**(math.floor(math.log(x/density,10)))
-        if x/thenth > density*10/2:
-            thenth = thenth*5
-        if x/thenth > density*10/4:
-            thenth = thenth*2
-        return thenth
-    
-    axs.set_xticks(np.arange(0, maxLength, stepSize(maxLength)))
-    mm = min(maxValue, yMax) if yMax is not None else maxValue
-    axs.set_yticks(np.arange(0, mm, stepSize(mm)))
-    axs.tick_params(axis='both', which='major', labelsize=15)
-    plt.xticks(rotation=90)
-    axs.grid()
-    
-    for i in range(len(graphs)):
-        g = graphs[i]
-        t = titles[i]
-        axs.plot(x, g, label=t)  # Plot some data on the axes.
-    axs.set_xlabel('Epochs', fontsize=20)  # Add an x-label to the axes.
-    axs.set_ylabel('Loss', fontsize=20)  # Add a y-label to the axes.
-    axs.set_title("")  # Add a title to the axes.
-    axs.legend(titles, fontsize=10)  # Add a legend.
-    
+    # Normalize
+    normalized_labels1 = (labels1-labels1.min())/(labels1.max()-labels1.min())
+    normalized_test1 = (test1-test1.min())/(test1.max()-test1.min())
+    return normalized_labels1, normalized_test1, targets1
+
+
+def polar_coordinates(labels, test):
+    # Make a copy
+    labels1_normed_r = labels.copy()
+    test1_normed_r = test.copy()
+
+    # Move origo to centre
+    labels1_normed_r['latitude'] = labels1_normed_r['latitude'] -  labels1_normed_r['latitude'].mean()
+    labels1_normed_r['longitude'] = labels1_normed_r['longitude'] -  labels1_normed_r['longitude'].mean()
+    test1_normed_r['latitude'] = test1_normed_r['latitude'] -  test1_normed_r['latitude'].mean()
+    test1_normed_r['longitude'] = test1_normed_r['longitude'] -  test1_normed_r['longitude'].mean()
+
+    # Convert to polar coordinates
+    labels1_normed_r['r'] =  np.sqrt(labels1_normed_r['latitude']**2 + labels1_normed_r['longitude']**2)
+    labels1_normed_r['theta'] = np.arctan(labels1_normed_r['longitude']/labels1_normed_r['latitude'])
+    test1_normed_r['r'] =  np.sqrt(test1_normed_r['latitude']**2 + test1_normed_r['longitude']**2)
+    test1_normed_r['theta'] = np.arctan(test1_normed_r['longitude']/test1_normed_r['latitude'])
+
+    return labels1_normed_r, test1_normed_r
