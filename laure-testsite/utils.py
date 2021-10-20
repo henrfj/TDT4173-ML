564757ca7bafe2e4528764d9079947d3c49c80f8
GoldenHuntingBear <beringer.lam@gmail.com>
2021-10-20 10:09:31 +0200

    New samples and new notebook
    New predictions by stacking RF algorithms

--- laure-testsite/utils.py
+++ laure-testsite/utils.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
 
 def fillnaReg(df, X_features, y_feature):
     df = df.copy()
@@ -10,3 +12,33 @@ def fillnaReg(df, X_features, y_feature):
     df[y_feature] = np.where(df[y_feature]>0, df[y_feature], predict)
     # X.pop('area_living_pred')
     return df
+
+def getAllMetadata():
+    metaData_apartment = pd.read_json('../data/apartments_meta.json')
+    metaData_building = pd.read_json('../data/buildings_meta.json')
+    metaData_apartment.at[0, 'name'] = 'apartment_id'
+    metaData_building.at[0, 'name'] = 'building_id'
+    metaData = pd.concat([metaData_apartment, metaData_building])
+    return metaData
+
+def getAllTrainData():
+    train_apartment = pd.read_csv('../data/apartments_train.csv')
+    train_building = pd.read_csv('../data/buildings_train.csv')
+    train = pd.merge(train_apartment, train_building.set_index('id'), how='left', left_on='building_id', right_on='id')
+    return train
+
+def getAllTestData():
+    test_apartment = pd.read_csv('../data/apartments_test.csv')
+    test_building = pd.read_csv('../data/buildings_test.csv')
+    test = pd.merge(test_apartment, test_building.set_index('id'), how='left', left_on='building_id', right_on='id')
+    return test
+
+def getFeatureImportanceGraph(model, X_train):
+    importances = model.feature_importances_
+    forest_importances = pd.Series(importances, index=X_train.columns)
+
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(ax=ax)
+    ax.set_title("MDI or Gini Importance")
+    ax.set_ylabel("Mean decrease in impurity")
+    fig.tight_layout()
